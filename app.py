fef2a6a9ae203bf5869373ee762d30d65e08ebef
Eduardo <eduardo.carneiro@seduc.ce.gov.br>
2024-10-16 14:28:50 -0300

    Commit inicial

--- app.py
+++ app.py
@@ -88,4 +88,4 @@ def edit_pessoa(nome):
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8001, debug=True)
+    app.run(host='0.0.0.0', port=8000, debug=True)
